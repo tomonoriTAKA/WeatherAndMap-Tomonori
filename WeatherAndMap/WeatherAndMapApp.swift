b328d1c311e6934a5d7f14861a22e9bc2ed9a71b
Tomonori <tomonori0204.takahashi@gmail.com>
2025-02-15 16:29:27 +0900

    Initial Commit

---
+++ WeatherAndMap/WeatherAndMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAndMapApp.swift
+//  WeatherAndMap
+//
+//  Created by 高橋知憲 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAndMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
